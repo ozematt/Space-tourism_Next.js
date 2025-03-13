98d9e9e477c89c8c65076e565637a658fa8469d2
Mateusz Obirek <neutralnie7@gmail.com>
2025-03-13 15:55:41 +0100

    edited slide menu

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -8,13 +8,13 @@ type NavLinkProps = {
 
 const NavLink = ({ href, number, name }: NavLinkProps) => {
   return (
-    <Link href={href} className="relative flex h-full items-center">
-      <li className="peer line-clamp-1">
+    <li className="relative flex h-full items-center">
+      <Link href={href} className="peer line-clamp-1">
         <span className="pr-1 font-bold">0{number}</span>
         {name}
-      </li>
+      </Link>
       <div className="absolute bottom-0 w-full border-2 opacity-0 transition duration-1000 ease-in-out peer-hover:opacity-100" />
-    </Link>
+    </li>
   );
 };
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import NavLink from "./NavLink";
@@ -44,7 +46,11 @@ const Navbar = () => {
           />
         ))}
       </ul>{" "}
-      <button className="peer cursor-pointer md:hidden">
+      <input type="checkbox" id="menu-toggle" className="peer hidden" />
+      <label
+        htmlFor="menu-toggle"
+        className="cursor-pointer peer-checked:hidden md:hidden"
+      >
         {" "}
         <Image
           src="/icons/icon-hamburger.svg"
@@ -52,26 +58,40 @@ const Navbar = () => {
           width={24}
           height={21}
         />
-      </button>
-      <div className="fixed top-0 right-0 z-20 flex h-full w-0 flex-col backdrop-blur-[40px] transition-all duration-300 ease-in-out peer-focus-within:w-[75%] md:hidden">
-        <Image
-          src="/icons/icon-close.svg"
-          alt="Close menu"
-          width={24}
-          height={21}
+      </label>
+      <div className="fixed top-0 right-0 z-20 flex h-full w-0 flex-col backdrop-blur-[40px] transition-all duration-300 ease-in-out peer-checked:w-[75%] md:hidden">
+        <label
+          htmlFor="menu-toggle"
           className="mx-6 my-8 cursor-pointer self-end"
-        />
+        >
+          {" "}
+          <Image
+            src="/icons/icon-close.svg"
+            alt="Close menu"
+            width={24}
+            height={21}
+          />
+        </label>
+
         <ul className="font-barlow-condensed mt-12 flex flex-col gap-8 tracking-[2px]">
           {" "}
           {navLinks.map((navLink, index) => (
-            <Link key={navLink.name} href={navLink.href} className="relative">
-              <li className="peer pl-8">
+            <li
+              key={navLink.name}
+              className="relative"
+              onClick={() => {
+                (
+                  document.getElementById("menu-toggle") as HTMLInputElement
+                ).checked = false;
+              }}
+            >
+              <Link href={navLink.href} className="peer pl-8">
                 <span className="pr-2 font-bold">0{index}</span>
                 {navLink.name}
-              </li>
+              </Link>
 
               <div className="absolute top-0 right-0 h-full border-2 opacity-0 transition duration-1000 ease-in-out peer-hover:opacity-100" />
-            </Link>
+            </li>
           ))}
         </ul>
       </div>
