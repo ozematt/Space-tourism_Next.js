c482e1958b5c833bcce781822e6bc90e690fdd1d
Mateusz Obirek <neutralnie7@gmail.com>
2025-03-13 15:15:09 +0100

    added destination media queries

--- src/app/destination/[planet]/page.tsx
+++ src/app/destination/[planet]/page.tsx
@@ -15,8 +15,8 @@ const PlanetPage = async ({ params }: PlanetPageProps) => {
         className="fixed inset-0 -z-1 h-screen w-screen object-cover"
       />
 
-      <div className="mx-40 my-12">
-        <h4 className="font-barlow-condensed text-[28px] tracking-[4px] uppercase">
+      <div className="mx-10 my-6 sm:my-12 lg:mx-40">
+        <h4 className="font-barlow-condensed text-center tracking-[4px] uppercase sm:text-left sm:text-xl lg:text-[28px]">
           <span className="pr-6 font-bold opacity-50">01</span> pick your
           destination
         </h4>
--- src/components/Destination.tsx
+++ src/components/Destination.tsx
@@ -14,17 +14,18 @@ type DestinationProps = {
 ``;
 const Destination = ({ planet }: DestinationProps) => {
   return (
-    <div className="mt-6 flex h-full max-h-[734px] justify-between py-32">
+    <div className="mt-6 flex h-full max-h-[734px] justify-between max-xl:flex-col max-xl:items-center sm:py-16 lg:py-32">
       {" "}
       <Image
         src={planets[planet].image}
         alt={planets[planet].imageAlt}
         width={480}
         height={480}
+        className="my-6 size-[150px] shrink-0 sm:size-[300px] lg:size-[480px]"
       />
-      <div className="w-full max-w-[539px] px-12">
+      <div className="w-full max-xl:text-center sm:px-[87px] lg:px-12 xl:max-w-[539px]">
         {/* planet navigation */}
-        <div className="flex gap-8">
+        <div className="flex gap-8 max-xl:justify-center sm:mt-20">
           {planetsArr.map((element) => (
             <Link
               key={element}
@@ -32,7 +33,7 @@ const Destination = ({ planet }: DestinationProps) => {
               className="relative"
             >
               <h6
-                className={`peer text-space-blue font-barlow-condensed tracking-[2px] uppercase hover:text-white`}
+                className={`peer text-space-blue font-barlow-condensed text-sm tracking-[2px] uppercase hover:text-white sm:text-base`}
               >
                 {element}
               </h6>
@@ -40,17 +41,18 @@ const Destination = ({ planet }: DestinationProps) => {
             </Link>
           ))}
         </div>
-        <h2 className="font-bellefair mt-10 text-8xl uppercase">
+
+        <h2 className="font-bellefair mt-6 text-[80px] uppercase lg:mt-10 lg:text-8xl">
           {planets[planet].name}
         </h2>
-        <p className="text-space-blue font-barlow pt-4 text-lg">
+        <p className="text-space-blue font-barlow pt-4 xl:text-lg">
           {planets[planet].description}
         </p>
         <div className="my-10 border-[0.5px] opacity-30" />
 
-        <div className="flex gap-6">
+        <div className="flex gap-6 max-[400px]:flex-col max-[400px]:items-center sm:justify-between">
           <div className="w-full max-w-[210px]">
-            <p className="font-barlow-condensed text-sm tracking-[2px] uppercase">
+            <p className="font-barlow-condensed text-space-blue text-sm tracking-[2px] uppercase">
               avg. distance
             </p>
             <p className="font-bellefair pt-1 text-[28px] uppercase">
@@ -58,7 +60,7 @@ const Destination = ({ planet }: DestinationProps) => {
             </p>
           </div>
           <div className="w-full max-w-[210px]">
-            <p className="font-barlow-condensed text-sm tracking-[2px] uppercase">
+            <p className="font-barlow-condensed text-space-blue text-sm tracking-[2px] uppercase">
               est. travel time
             </p>
             <p className="font-bellefair pt-1 text-[28px] uppercase">
