a66674948822d0863256fd5434fbda7cc8233460
Mateusz Obirek <neutralnie7@gmail.com>
2025-03-17 13:55:55 +0100

    added media queries

--- src/app/technology/[stage]/page.tsx
+++ src/app/technology/[stage]/page.tsx
@@ -13,24 +13,26 @@ const StagePage = async ({ params }: StagePageProps) => {
   return (
     <section className="h-screen">
       <SectionBackground />
+
       <div className="mx-10 my-6 sm:my-12 lg:mx-40">
         <SectionTitle number="03" name="space launch 101" />
-      </div>{" "}
-      <div className="flex justify-between">
+      </div>
+
+      <div className="flex h-full items-center max-2xl:flex-col-reverse 2xl:max-h-[734px] 2xl:justify-between">
         {/* stage */}
-        <div className="ml-40 flex xl:pt-50">
-          {" "}
-          <div className="space-y-8 pr-16">
+        <div className="h-full px-6 pt-8 lg:px-0 2xl:ml-40 2xl:flex 2xl:shrink-0 2xl:pt-45">
+          <div className="justify-center gap-7 space-y-8 max-2xl:flex 2xl:pr-16">
             {technologyStageArr.map((link, index) => (
               <Link
                 href={`/technology/${link}`}
                 key={index}
-                className={` ${stage === link ? "bg-white text-blue-950" : ""} font-bellefair grid size-20 cursor-pointer place-items-center rounded-full text-[32px] ring-1 ring-white transition-all duration-300 ease-in-out hover:bg-white hover:text-blue-950`}
+                className={` ${stage === link ? "bg-white text-blue-950" : ""} font-bellefair grid size-14 cursor-pointer place-items-center rounded-full text-2xl ring-1 ring-white transition-all duration-300 ease-in-out hover:bg-white hover:text-blue-950 lg:size-20 lg:text-[32px]`}
               >
                 {index + 1}
               </Link>
             ))}
           </div>
+
           <TechnologyInfo stage={stage} />
         </div>
 
@@ -39,7 +41,14 @@ const StagePage = async ({ params }: StagePageProps) => {
           alt="space rocket image"
           width={608}
           height={600}
-          className="object-contain"
+          className="object-contain max-2xl:hidden"
+        />
+        <Image
+          src={technologyStage[stage].imageLandscape}
+          alt="space rocket image"
+          width={770}
+          height={356}
+          className="object-cover object-top max-lg:h-[258px] 2xl:hidden"
         />
       </div>
     </section>
