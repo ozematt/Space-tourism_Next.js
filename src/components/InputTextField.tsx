8a1c24772922e911f61ea81c64bf34dcb2a534bb
Mateusz Obirek <neutralnie7@gmail.com>
2025-03-19 20:41:12 +0100

    added inputs

--- src/components/InputTextField.tsx
+++ src/components/InputTextField.tsx
@@ -1,5 +1,29 @@
-const InputTextField = () => {
-  return <div>InputTextField</div>;
+type InputTextFieldProps = {
+  id: string;
+  placeholder: string;
+};
+
+const InputTextField = ({ id, placeholder }: InputTextFieldProps) => {
+  return (
+    <div className="relative">
+      <input
+        type="text"
+        id={id}
+        placeholder={placeholder}
+        className="peer h-[50px] w-full rounded-[5px] pl-4 placeholder-transparent ring-1 ring-white focus:ring-2 focus:ring-white focus:outline-none"
+      />
+      <label
+        htmlFor={id}
+        className="font-barlow-condensed pointer-events-none absolute top-[-28px] left-0 text-base tracking-[0.5px] transition-all select-none peer-placeholder-shown:top-[9px] peer-placeholder-shown:left-4.5 peer-placeholder-shown:text-xl"
+      >
+        {placeholder}
+      </label>
+      {/* {error && */}
+      <p className="font-barlow-condensed absolute top-[-28px] right-0 text-base tracking-[.5px] text-red-500">
+        Error
+      </p>
+    </div>
+  );
 };
 
 export default InputTextField;
--- src/components/Reservation.tsx
+++ src/components/Reservation.tsx
@@ -1,3 +1,5 @@
+import { InputTextField } from ".";
+
 const Reservation = () => {
   return (
     <div className="mx-auto mt-[100px] flex h-full max-h-[600px] w-full max-w-[945px] ring-1 ring-white/20 backdrop-blur-[10px]">
@@ -12,29 +14,10 @@ const Reservation = () => {
           </p>
         </div>
         {/* INPUTS */}
-        <div className="mt-[67px]">
-          {/* NAME */}
-          <div className="relative">
-            <input
-              type="text"
-              id="name"
-              placeholder="Name"
-              className="peer h-[50px] w-full rounded-[5px] pl-4 placeholder-transparent ring-1 ring-white focus:ring-2 focus:ring-white focus:outline-none"
-            />
-            <label
-              htmlFor="name"
-              className="font-barlow-condensed pointer-events-none absolute top-[-28px] left-0 text-base tracking-[0.5px] transition-all select-none peer-placeholder-shown:top-[9px] peer-placeholder-shown:left-4.5 peer-placeholder-shown:text-xl"
-            >
-              Name
-            </label>
-            {/* {error && */}
-            <p className="font-barlow-condensed absolute top-[-28px] right-0 text-base tracking-[.5px] text-red-500">
-              Error
-            </p>
-          </div>
-
-          <input type="text" />
-          <input type="text" />
+        <div className="mt-[67px] space-y-12">
+          <InputTextField id="name" placeholder="Name" />
+          <InputTextField id="email" placeholder="Email Address" />
+          <InputTextField id="phone" placeholder="Phone Number" />
         </div>
       </div>
     </div>
