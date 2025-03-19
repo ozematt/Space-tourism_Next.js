66287e4cf7d8857922a81e3aefa56ea7f0e07e83
Mateusz Obirek <neutralnie7@gmail.com>
2025-03-19 21:11:10 +0100

    addde data

--- src/data/reserve.ts
+++ src/data/reserve.ts
@@ -11,4 +11,24 @@ const steps = [
     title: "personal info",
     description: "Please provide your name, email, and phone number.",
   },
+  {
+    step: 2,
+    title: "pick your destination",
+    description: "You have the options. Choose wisely where you want to go.",
+  },
+  {
+    step: 3,
+    title: "choose add-ons",
+    description: "Add-ons help enhance your space experience.",
+  },
+  {
+    step: 4,
+    title: "personal info",
+    description: "Please provide your name, email, and phone number.",
+  },
+  {
+    step: 5,
+    title: "personal info",
+    description: "Please provide your name, email, and phone number.",
+  },
 ];
