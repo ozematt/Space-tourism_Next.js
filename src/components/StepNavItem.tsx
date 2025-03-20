73fb3dc56e58ed0bebad7ee117d516aeece53d62
Mateusz Obirek <neutralnie7@gmail.com>
2025-03-20 20:13:22 +0100

    deleted component

--- src/components/StepNavItem.tsx
+++ src/components/StepNavItem.tsx
@@ -3,24 +3,4 @@ type StepNavItemProps = {
   title: string;
 };
 
-const StepNavItem = ({ stepNumber, title }: StepNavItemProps) => {
-  return (
-    <div className="flex items-center">
-      <div className="font-bellefair grid size-[34px] place-items-center rounded-full uppercase ring-1 ring-white hover:bg-white hover:text-black">
-        {stepNumber}
-      </div>
-      <div className="space-y-4 pl-4">
-        {" "}
-        <p className="font-barlow-condensed text-xs leading-[1.2px] tracking-[1.5px] uppercase">
-          {" "}
-          step 0{stepNumber}
-        </p>
-        <p className="font-barlow leading-[1.2px] font-bold tracking-[2px] uppercase">
-          {title}
-        </p>
-      </div>
-    </div>
-  );
-};
-
 export default StepNavItem;
