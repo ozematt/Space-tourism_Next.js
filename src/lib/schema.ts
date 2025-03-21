b006c696486c7f36bec6c0beb9643a645bea4797
Mateusz Obirek <neutralnie7@gmail.com>
2025-03-21 18:27:13 +0100

    addde schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -9,3 +9,11 @@ export const stepOneSchema = z.object({
 export const stepTwoSchema = z.object({
   destination: z.enum(["moon", "mars", "europa", "titan"]),
 });
+
+export const stepThreeSchema = z.object({
+  addOns: z.object({
+    lunarHotel: z.boolean(),
+    marsColony: z.boolean(),
+    titanCamp: z.boolean(),
+  }),
+});
