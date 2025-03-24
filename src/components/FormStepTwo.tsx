29861d19787fc1e28f8b7620fb387de64817bffe
Mateusz Obirek <neutralnie7@gmail.com>
2025-03-24 19:18:12 +0100

    add planet checkboxes to FormStepTwo component

--- src/components/FormStepTwo.tsx
+++ src/components/FormStepTwo.tsx
@@ -1,1 +1,14 @@
+import { planetsArr } from "@/data";
+import InputPlanetCheckbox from "./InputPlanetCheckbox";
+
+const FormStepTwo = () => {
+  return (
+    <div className="middle:grid-cols-2 grid grid-cols-1 gap-4">
+      {planetsArr.map((planet) => (
+        <InputPlanetCheckbox key={planet} planet={planet} />
+      ))}
+    </div>
+  );
+};
+
 export default FormStepTwo;
