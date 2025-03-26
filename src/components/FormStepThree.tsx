062061479559afc7d816319be48dd3c86e4b4a5c
Mateusz Obirek <neutralnie7@gmail.com>
2025-03-26 10:39:19 +0100

    refactor: update FormStepThree component with improved checkbox handling and add tick SVG

--- src/components/FormStepThree.tsx
+++ src/components/FormStepThree.tsx
@@ -1,17 +1,17 @@
 const FormStepThree = () => {
   return (
     <div className="hover:bg-opacity-20 translate-all middle:h-[82px] relative flex cursor-pointer flex-col rounded-[5px] p-4 ring-[.5px] duration-500 ease-in-out hover:bg-white/10">
-      
-      {/* <div className="my-auto">
+      <div className="my-auto flex items-center">
         <input
           type="checkbox"
-          id="addOns"
-          className="indeterminate:bg-gray-800"
+          value="1"
+          className="size-6 appearance-none rounded-[5px] bg-center bg-no-repeat ring-1 checked:bg-green-600 checked:bg-[url(/reserve/tick.svg)]"
         />
+
         <label htmlFor="addOns" className="pl-6">
-          Label{" "}
+          Label
         </label>
-      </div> */}
+      </div>
     </div>
   );
 };
