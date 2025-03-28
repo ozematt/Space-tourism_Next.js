8a8063233d506289c89644493f45544d51ba6578
Mateusz Obirek <neutralnie7@gmail.com>
2025-03-28 13:07:21 +0100

    deleted logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,15 +3,7 @@ import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Link href="/" className="shrink-0 px-0 min-[850px]:px-[40px] lg:px-[64px]">
-      <Image
-        src="/icons/logo.svg"
-        alt="logo"
-        width={40}
-        height={40}
-        className="md:size-12"
-      />
-    </Link>
+
   );
 };
 
