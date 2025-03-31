68669b771cf803a506b290545d10bb27a272020a
Mateusz Obirek <neutralnie7@gmail.com>
2025-03-31 22:17:37 +0200

    add crew member links to CrewNavbar component

--- src/components/CrewNavbar.tsx
+++ src/components/CrewNavbar.tsx
@@ -7,7 +7,15 @@ const CrewNavbar = () => {
   const pathname = usePathname();
   const isActive = pathname.split("/")[2];
   return (
-   
+    <div className="flex justify-center gap-4 pt-15 xl:justify-start xl:gap-10 xl:pt-9">
+      {crewMembersArr.map((crewMember) => (
+        <Link href={"/crew/" + crewMember} key={crewMember}>
+          <div
+            className={`size-2.5 xl:size-4 ${isActive === crewMember ? "opacity-100" : ""} cursor-pointer rounded-full bg-white opacity-50 hover:opacity-100`}
+          />
+        </Link>
+      ))}
+    </div>
   );
 };
 
