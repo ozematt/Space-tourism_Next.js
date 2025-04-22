e14438559ec01db9da3214022638ed973af0167b
Mateusz Obirek <neutralnie7@gmail.com>
2025-04-22 18:22:18 +0200

    refactor

--- src/components/StepNavItem.tsx
+++ src/components/StepNavItem.tsx
@@ -13,11 +13,6 @@ type StepNavItemProps = {
 };
 
 const StepNavItem = ({ stepNumber, item }: StepNavItemProps) => {
-  const pathname = usePathname();
-  const isActive = pathname === `/reserve/step0${stepNumber}`;
-
-  const { title } = item;
-
   return (
     <div className="flex items-center">
       <div
